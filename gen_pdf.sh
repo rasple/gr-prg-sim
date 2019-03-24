10244a17ea5fdb876c12ce666b25ffabb37ee0c3
rasple <frankme97@gmail.com>
2019-03-24 23:21:43 +0100

    updated readme + images

--- gen_pdf.sh
+++ gen_pdf.sh
@@ -1,2 +1,2 @@
 #!/bin/env sh
-pandoc --toc -V colorlinks -f markdown-implicit_figures README.md -o README.pdf
+pandoc -V fontsize=11pt --toc -V colorlinks -f markdown+implicit_figures README.md -o README.pdf
